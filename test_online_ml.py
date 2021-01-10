bffe491a52526e5fb73f8b4232b6c59fadfd894e
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 19:28:42 +0100

    adding test for read csv

--- test_online_ml.py
+++ test_online_ml.py
@@ -1,5 +1,6 @@
 import utils
 import urllib
+import pandas as pd
 
 def test_url_validate():
 	assert utils.url_validate('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv') == True
@@ -20,4 +21,13 @@ def test_http_check4():
 	assert utils.http_check('123') == False
 def test_http_check5():
 	assert utils.http_check(3) == False
+
+def test_read_csv1():
+	assert isinstance(utils.read_csv('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv'),pd.DataFrame) == True
+def test_http_check2():
+	assert isinstance(utils.read_csv(''),pd.DataFrame)== False
+def test_http_check3():
+	assert isinstance(utils.read_csv('123'),pd.DataFrame) == False
+def test_http_check4():
+	assert isinstance(utils.read_csv(3),pd.DataFrame) == False
 
