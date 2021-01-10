affcbcee34c226fe51250bb9f5d4156c126a18e6
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 19:05:22 +0100

    fixing naming convention

--- test_online_ml.py
+++ test_online_ml.py
@@ -1,12 +1,12 @@
 import utils
 import urllib
 
-def test_check_http():
+def test_url_validate():
 	assert utils.url_validate('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv') == True
-def test_check_http1():
+def test_url_validate1():
 	assert utils.url_validate('dd') == False
-def test_check_http2():
+def test_url_validate2():
 	assert utils.url_validate('') == False
-def test_check_http3():
+def test_url_validate3():
 	assert utils.url_validate(3) == False
 
