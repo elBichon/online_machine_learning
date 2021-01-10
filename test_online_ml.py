23891548ce27054b7124cb33418644abe8f6f1e4
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 19:12:54 +0100

    adding http check test

--- test_online_ml.py
+++ test_online_ml.py
@@ -10,3 +10,14 @@ def test_url_validate2():
 def test_url_validate3():
 	assert utils.url_validate(3) == False
 
+def test_http_check1():
+	assert utils.http_check('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv') == True
+def test_http_check2():
+	assert utils.http_check('https://raw.githubusercontent.com/azertyuiop') == False
+def test_http_check3():
+	assert utils.http_check('') == False
+def test_http_check4():
+	assert utils.http_check('123') == False
+def test_http_check5():
+	assert utils.http_check(3) == False
+
