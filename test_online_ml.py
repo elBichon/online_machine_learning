9bcba87784f3e1bdf91e706e69bed52941813b85
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 23:26:43 +0100

    Update test_online_ml.py

--- test_online_ml.py
+++ test_online_ml.py
@@ -3,7 +3,7 @@ import urllib
 import pandas as pd
 
 def test_url_validate():
-	assert utils.url_validate('https://raw.githubusercontent.com/elBichon/online_machine_learning/main/titanic/train.csv') == True
+	assert utils.url_validate('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv') == True
 def test_url_validate1():
 	assert utils.url_validate('dd') == False
 def test_url_validate2():
@@ -12,7 +12,7 @@ def test_url_validate3():
 	assert utils.url_validate(3) == False
 
 def test_http_check1():
-	assert utils.http_check('https://raw.githubusercontent.com/elBichon/online_machine_learning/main/titanic/train.csv') == True
+	assert utils.http_check('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv') == True
 def test_http_check2():
 	assert utils.http_check('https://raw.githubusercontent.com/azertyuiop') == False
 def test_http_check3():
@@ -23,26 +23,13 @@ def test_http_check5():
 	assert utils.http_check(3) == False
 
 def test_read_csv1():
-	assert isinstance(utils.read_csv('https://raw.githubusercontent.com/elBichon/online_machine_learning/main/titanic/train.csv'),pd.DataFrame) == True
+	assert isinstance(utils.read_csv('https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv'),pd.DataFrame) == True
 def test_http_check2():
 	assert isinstance(utils.read_csv(''),pd.DataFrame)== False
 def test_http_check3():
 	assert isinstance(utils.read_csv('123'),pd.DataFrame) == False
 def test_http_check4():
 	assert isinstance(utils.read_csv(3),pd.DataFrame) == False
-
-
-def test_remove_unique_feature1():
-	df = utils.read_csv('https://raw.githubusercontent.com/elBichon/online_machine_learning/main/titanic/train.csv')
-	assert isinstance(utils.remove_unique_feature(df,pd.DataFrame) == True
-def test_remove_unique_feature2():
-	assert isinstance(utils.remove_unique_feature('',pd.DataFrame) == False
-def test_remove_unique_feature3():
-	assert isinstance(utils.remove_unique_feature('12132',pd.DataFrame) == False
-def test_remove_unique_feature3():
-	data = {'col_1': [3, 2, 1, 0]}
-	df = pd.DataFrame.from_dict(data)
-	assert isinstance(utils.remove_unique_feature(3,pd.DataFrame) == False
 
 
 def test_remove_unique_feature1():
