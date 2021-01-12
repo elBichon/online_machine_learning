5e881e422d5ac1a9e9e1c13ba660fce6255be787
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-12 21:08:07 +0100

    adding missing imports

--- utils.py
+++ utils.py
@@ -8,6 +8,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from tpot import TPOTRegressor
 from tpot import TPOTClassifier
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 
 # URL-link validation
 ip_middle_octet = u"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5]))"
