0d812f9c12ca8f9ada4350e4bf8ed36ba754c7ba
elBichon <reuglewiczjeanedouard@gmail.com>
2021-02-19 10:10:34 +0100

    more trimming for heroku

--- app.py
+++ app.py
@@ -91,12 +91,12 @@ def result_page():
 						df[text_field] = [','.join(map(str, l)) for l in df[text_field]]
 						df[text_field] = df[text_field].str.replace(',',' ')
 						df[text_field] = df[text_field].apply(lambda x: " ".join([y.lemma_ for y in en_core(x)]))
-						vectorizer = CountVectorizer(analyzer = "word",tokenizer = None, preprocessor = None, stop_words = None,   max_features = 5000) 
+						vectorizer = CountVectorizer(analyzer = "word",tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000) 
 						train_data_features = vectorizer.fit_transform(df[text_field].values.tolist()[0:10])
 						train_data_features = train_data_features.toarray()[0:10]
 						forest = RandomForestClassifier(random_state=42)
-						param_grid = {'n_estimators': [100],'max_features': ['auto'],'max_depth' : [5],'criterion' :['gini']}
-						forest = GridSearchCV(estimator=forest, param_grid=param_grid, cv= 5)
+						param_grid = {'n_estimators': [10],'max_features': ['auto'],'max_depth' : [5],'criterion' :['gini']}
+						forest = GridSearchCV(estimator=forest, param_grid=param_grid, cv= 3)
 						forest = forest.fit(train_data_features, df[label][0:10])
 						params = forest.best_params_
 						result = 'the best parameters are: ' + str(params)
